50d21ba36918fabc146ca7ac6491f7576f4823d3
Ignacio Baixas <iobaixas@gmail.com>
2018-09-10 11:33:18 -0300

    feat(ui/IdentityBar): adds avatar and alias
    Also improves visual style

--- app/javascript/components/identity_bar.vue
+++ app/javascript/components/identity_bar.vue
@@ -1,22 +1,45 @@
 <template>
   <div class="identity-bar">
     <div v-if="idKey">
-      {{ idKey }}
+      <img class="identity-bar__avatar" :src="avatarUrl"/>
+
+      <div class="identity-bar__name">
+        <div class="identity-bar__alias">
+          {{ aliasKey }}
+        </div>
+
+        <div class="identity-bar__key">
+          {{ splitKey }}
+        </div>
+      </div>
     </div>
-    <div v-else>
-      Anonymous
+    <div v-else class="identity-bar__anonymous">
+      ANONYMOUS
     </div>
   </div>
 </template>
 
 <script>
 import signature from '../services/signature_service';
+import bs58 from 'bs58';
 
 export default {
   data: () => ({
     idKey: null,
   }),
   props: ['secret'],
+  computed: {
+    avatarUrl() {
+      return `https://robohash.org/${this.idKey}`;
+    },
+    aliasKey() {
+      const b58id = bs58.encode(Buffer.from(this.idKey, 'hex')).toUpperCase().substr(0, 8);
+      return `X-${b58id}`;
+    },
+    splitKey() {
+      return `0x${this.idKey.substr(0, 32)}\n${this.idKey.substr(32, 66)}`
+    }
+  },
   watch: {
     secret: {
       immediate: true,
@@ -31,8 +54,40 @@ export default {
   },
 };
 </script>
-
 <style lang="scss">
 .identity-bar {
+  position: relative;
+  overflow: hidden;
+  background: #186573;
+
+  &__avatar {
+    transform: scaleX(-1);
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    height: 100px;
+  }
+
+  &__name {
+    padding: 5px;
+    margin-left: 90px;
+  }
+
+  &__alias {
+    font-size: 30px;
+    color: rgba(255, 255, 255, 0.75);
+  }
+
+  &__key {
+    font-family: monospace;
+    font-size: 14px;
+    color: rgba(255, 255, 255, 0.5);
+  }
+
+  &__anonymous {
+    padding: 5px 10px;
+    font-size: 20px;
+    color: rgba(255, 255, 255, 0.75);
+  }
 }
 </style>
